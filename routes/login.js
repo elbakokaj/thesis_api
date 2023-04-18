1e6e300795e1aa13a434a657a3487e6d7b1c28a8
ElbaKokaj <elbaaak@gmail.com>
2023-04-18 23:48:28 +0200

    shfaqja e studentave per attendance edhe ndrrimi i statusit

--- routes/login.js
+++ routes/login.js
@@ -4,22 +4,34 @@ const sign = require('jwt-encode');
 const jwt_decode = require('jwt-decode');
 
 const Users = require("../models/users");
+const Courses = require("../models/courses");
 
 
 router.post("/", async (req, res) => {
     try {
         const foundUser = await Users.findOne({ email: req.body.email });
+
         var passDecode = jwt_decode(req.body.pass);
-        console.log("passdecode1231223123123arton", foundUser)
+        // var passDecode1 = jwt_decode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MmVkZWUzY2VmYzU3ZTM4ZmEzNDU5ZSIsInJvbGUiOiJwcm9mZXNzb3IiLCJlbWFpbCI6InByb2Zlc3NvckBleGFtcGxlLmNvbSJ9.hla4XfgiTPVK4GR5YsVgH35AHz8h0VPhEnoZw7o7GG0");
+        console.log("passdecode1231223123123arton", passDecode)
         if (foundUser?.password == passDecode) {
             const secret = 'marinairPopaj';
+            const professor_id = foundUser?._id
+            const test = []
+            if (foundUser?.role == "professor") {
+                const foundCourse = await Courses.findOne({ professorId: professor_id });
+                const myjson = foundCourse?._id
+                test.push(myjson)
+            }
+            console.log('test', test)
             const data = {
                 id: foundUser?._id,
                 role: foundUser?.role,
-                email: foundUser?.email
+                email: foundUser?.email,
+                course_id: test[0]
             }
             let jwt = sign(data, secret);
-            console.log("tokeni qe me duhet", jwt);
+            // console.log("tokeni qe me duhet", jwt);
             // console.log(decoded);
             res.json({ token: jwt });
 
--- routes/professors/attendances.js
+++ routes/professors/attendances.js
@@ -15,14 +15,30 @@ router.get("/find_attendances", async (req, res) => {
 });
 
 
-router.get("/find_specific_status", async (req, res) => {
+router.post("/find_students_attendences", async (req, res) => {
+    console.log('req.query', req)
+    console.log('req.body', req.body)
     try {
-        const allAttendances = await Attendances.findOne({ courseId: req.query.course_id, date: req.query.course_date });
-        const records = allAttendances?.records?.filter((el) => el.studentId == req.query.student_id);
-        // 
+        const allAttendances = await Attendances.findOne({ courseId: req.body.course_id });
+        const allUsers = await Users.find();
 
-        console.log('attendance records for student', req.query.course_date);
-        res.json(records);
+        const records = allAttendances?.records?.filter((el) => el.studentId);
+        // console.log('records', records)
+        const all_students_that_attend = [];
+        for (const element of records) {
+            const student = allUsers.find((user) => user._id.toString() === element.studentId.toString());
+            if (student) {
+                var name = student?.firstName
+                var status = element
+                const myJSON = {
+                    name, status
+                }
+                all_students_that_attend.push(myJSON);
+            }
+
+        }
+        res.json(all_students_that_attend);
+
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal Server Error" });
